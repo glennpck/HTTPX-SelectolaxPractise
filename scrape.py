8eb2fa3f62319ada6f4b0a4933bacb860533eba8
glennpck <glennchia43@gmail.com>
2023-10-06 12:56:02 +0800

    parsing data into dict

--- scrape.py
+++ scrape.py
@@ -14,10 +14,24 @@ def get_html(page):
     resp = httpx.get(url)
     return HTMLParser(resp.text)
 
+def parse_products(html):
+    products = html.css('div.product')
+    results = []
+    for item in products:
+        new_item = Product(
+            manufacturer=item.css_first("span.title__manufacturer").text(),
+            title=item.css_first("span.title__name").text(),
+            price=item.css_first("div.product__price").text().strip()
+        )
+        results.append(asdict(new_item))
+
+    return results
+
 def main():
     for i in range(1, 4):
         html = get_html(i)
-        print(html.css_first("title").text())
+        res = parse_products(html)
+        print(res)
 
 if __name__ == "__main__":
     main()
