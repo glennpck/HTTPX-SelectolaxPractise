08c7bb9d9bf1eb046b21ba237cc9f29955aeafef
glennpck <glennchia43@gmail.com>
2023-10-12 13:54:28 +0800

    scraping success

--- request-json.py
+++ request-json.py
@@ -1,13 +1,44 @@
-import requests
 import httpx
-from bs4 import BeautifulSoup
 from selectolax.parser import HTMLParser
+
+class Book:
+    def __init__(self, isbn, title, author, cover, type, price, url):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.cover = cover
+        self.type = type
+        self.price = price
+        self.url = url
+
+url = "https://blackwells.co.uk/bookshop/search/?keyword=eighty+six"
 
 base_url = "https://blackwells.co.uk"
 
-url = "https://wordery.com/search?term=Eighty+Six+Light+Novel"
-
 resp = httpx.get(url)
 parse = HTMLParser(resp.text)
-for book in parse.css("li.o-book-list__book"):
-    print(book.css_first("a.c-book__title").text())
+for book in parse.css("li.search-result__item "):
+    div_check = book.css("a.product-name")
+    book_title = ''
+    for node in div_check:
+        try:
+            validateValue = node.attributes['itemprop']
+            book_title = node.text()
+            print(book_title)
+        except KeyError:
+            pass
+    booktest = Book(
+        isbn = book.css_first("a.btn").attributes['data-isbn'],
+        title = book_title,
+        author = book.css_first("p.product-author").text(),
+        cover = base_url + str(book.css_first("img").attributes['src']),
+        type = "Paperback",
+        price = book.css_first("li.product-price--current").text().strip(),
+        url = base_url + str(book.css_first("a.product-name").attributes["href"])
+    )
+    print(booktest.title)
+    print(booktest.author)
+    print(booktest.cover)
+    print(booktest.price)
+    print(booktest.url)
+    print("")
--- scrape.py
+++ scrape.py
@@ -1,41 +1,11 @@
 import httpx
 from selectolax.parser import HTMLParser
 
-from dataclasses import dataclass, asdict
+base_url = "https://blackwells.co.uk"
 
-@dataclass
-class Product:
-    image: str
-    title: str
-    price: str
-    num_sold: str
+url = "https://wordery.com/search?term=Eighty+Six+Light+Novel"
 
-def get_html():
-    url = f'https://shopee.sg/search?keyword=nike'
-    resp = httpx.get(url)
-    return HTMLParser(resp.text)
-
-def parse_products(html):
-    products = html.css('div.VTjd7p whIxGK')
-    results = []
-    for item in products:
-        text_title = item.css_first("div.ie3A+n bM+7UW Cve6sh").text()
-        print(text_title)
-        new_item = Product(
-            image=item.css_first("img._7DTxhh tWoeMk").text(),
-            title=item.css_first("div.ie3A+n bM+7UW Cve6sh").text(),
-            price=item.css_first("span.ZEgDH9").text(),
-            num_sold=item.css_first("div.r6HknA uEPGHT").text()
-        )
-        results.append(asdict(new_item))
-
-    return results
-
-def main():
-    html = get_html()
-    print(html)
-    res = parse_products(html)
-    ##print(res)
-
-if __name__ == "__main__":
-    main()
+resp = httpx.get(url)
+parse = HTMLParser(resp.text)
+for book in parse.css("li.o-book-list__book"):
+    print(book.css_first("a.c-book__title").text())
