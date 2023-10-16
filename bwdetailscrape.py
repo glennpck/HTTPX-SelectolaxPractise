4f8a569a278edf96796573cd97e174a21585b1ec
glennpck <glennchia43@gmail.com>
2023-10-16 13:34:55 +0800

    experimentation

--- bwdetailscrape.py
+++ bwdetailscrape.py
@@ -1,22 +1,23 @@
 import httpx
 from selectolax.parser import HTMLParser
 
+def get_title(item):
+    print(item.css_first("img").attributes)
 
-url = "https://blackwells.co.uk/bookshop/product/86-Eighty-Six-8-by-Asato-Asato-author-Shirabii-illustrator/9781975320768"
+
+url = "https://blackwells.co.uk/bookshop/search/?keyword=spy+classroom"
 
 base_url = "https://blackwells.co.uk"
 
 resp = httpx.get(url)
 parse = HTMLParser(resp.text)
-page = parse.css("div.container--50")
+for item in parse.css("li.search-result__item "):
+    get_title(item)
 
-title_list = page[0].css_first("h1.product__name").text(strip=True, deep=False).split(" ")
-title = ""
-for word in title_list:
-    if "\t" not in word:
-        title += word
+# author_list = page[0].css("p.product__author > a")
+# for node in author_list:
+#     print(node.text(strip=True, deep=False))
 
-print(title)
 # plist = page[1].css("p")
 # if page[1].css_first("font"):
 #     print(page[1].css_first("font").text(strip=True, deep=False))
