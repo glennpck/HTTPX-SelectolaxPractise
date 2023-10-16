0d63fa746be7104b1f771b0cb18b08d628a671eb
glennpck <glennchia43@gmail.com>
2023-10-16 10:46:09 +0800

    specific detail scraping

--- bwdetailscrape.py
+++ bwdetailscrape.py
@@ -10,14 +10,19 @@ resp = httpx.get(url)
 parse = HTMLParser(resp.text)
 page = parse.css("div.container--50")
 
-print(page[0].css_first("h1.product__name").text(strip=True, deep=False))
-plist = page[1].css("p")
-print(len(plist))
-if page[1].css_first("font"):
-    print(page[1].css_first("font").text(strip=True, deep=False))
-elif len(plist) == 1:
-    print(plist[0].text(deep=False, strip=True))
-elif len(plist) == 2:
-    print(plist[0].text(deep=False, strip=True))
-else:
-    print(plist[1].text(strip=True, deep=False))
+title_list = page[0].css_first("h1.product__name").text(strip=True, deep=False).split(" ")
+title = ""
+for word in title_list:
+    if "\t" not in word:
+        title += word
+
+print(title)
+# plist = page[1].css("p")
+# if page[1].css_first("font"):
+#     print(page[1].css_first("font").text(strip=True, deep=False))
+# elif len(plist) == 1:
+#     print(plist[0].text(deep=False, strip=True))
+# elif len(plist) == 2:
+#     print(plist[0].text(deep=False, strip=True))
+# else:
+#     print(plist[1].text(strip=True, deep=False))
